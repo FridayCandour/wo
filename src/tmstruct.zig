3c50073dcf300e0fba79c24e6a766afcdb34271e
fridaycandour <fridaymichaels662@gmail.com>
2023-06-03 18:37:36 +0100

    pub fn startUpdateInterface is walking

--- src/tmstruct.zig
+++ src/tmstruct.zig
@@ -7,6 +7,98 @@ const fetch = f.fetch;
 /// [x] tests
 //
 
+// All Evil Types here XD
+
+const Update = struct {
+    update_id: u32,
+    message: Message,
+};
+
+const Message = struct {
+    message_id: u32,
+    from: User,
+    chat: Chat,
+    date: u32,
+    text: []const u8,
+    entities: []Entity,
+};
+
+const User = struct {
+    id: u32,
+    is_bot: bool,
+    first_name: []const u8,
+    username: []const u8,
+    language_code: []const u8,
+};
+
+const Chat = struct {
+    id: u32,
+    first_name: []const u8,
+    username: []const u8,
+    type: []const u8,
+};
+
+const Entity = struct {
+    offset: u32,
+    length: u32,
+    type: []const u8,
+};
+
+const my_json_sample =
+    \\{
+    \\ "ok":true,"result":[
+    \\{"update_id":157100682,
+    \\"message": { 
+    \\"message_id": 6,
+    \\"from": { 
+    \\"id": 1233058250, 
+    \\"is_bot":false, 
+    \\"first_name": "Friday Candour [ask to pm]", 
+    \\"username":"Procal",
+    \\"language_code": "en"},
+    \\"chat": {  
+    \\"id":1233058250, 
+    \\"first_name": "Friday Candour [ask to pm]",
+    \\"username":"Procal",
+    \\"type":"private"
+    \\},
+    \\"date":1685808524,
+    \\"text":"/start",
+    \\"entities": [
+    \\{
+    \\"offset": 0,
+    \\"length":6,
+    \\"type": "bot_command"
+    \\}
+    \\]
+    \\ }
+    \\ }
+    \\ ]
+    \\}
+;
+
+const Config = struct {
+    ok: bool,
+    result: []Update,
+};
+
+fn fromJson(x: []const u8, alloc: std.mem.Allocator) !Config {
+    // desirialisation
+    var stream = std.json.TokenStream.init(x);
+    const parsedData = try std.json.parse(Config, &stream, .{ .allocator = alloc });
+    return parsedData;
+}
+
+pub fn proccess(data: []u8) !void {
+    // std.debug.print("{any}", .{data});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    const struc = try fromJson(data, allocator);
+    std.debug.print("\n {any}", .{struc});
+    defer std.json.parseFree(Config, struc, .{ .allocator = allocator });
+    try std.testing.expect(struc.ok == true);
+}
+
 pub const Bot = struct {
     token: []const u8,
     allocator: std.mem.Allocator,
@@ -56,14 +148,10 @@ pub const Bot = struct {
         while (true) {
             const data = try fetch(self.allocator, path, .POST);
             if (@TypeOf(data) == []u8) {
-                try self.proccess(data);
+                try proccess(data);
             }
             // Delay between requests to avoid hitting API limits
             std.time.sleep(1000);
         }
-    }
-    pub fn proccess(self: *const Self, data: []u8) !void {
-        _ = self;
-        std.debug.print("{any}", .{data});
     }
 };
